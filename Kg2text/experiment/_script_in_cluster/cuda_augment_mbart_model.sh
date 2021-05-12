185c019d91f701325392ac8da2a68fbc0b619065
Jiayi Xian <xianjiay@amazon.com>
2021-05-12 15:31:15 +0000

    finetune sct and bped data wo eos w/otags done

--- Kg2text/experiment/_script_in_cluster/cuda_augment_mbart_model.sh
+++ Kg2text/experiment/_script_in_cluster/cuda_augment_mbart_model.sh
@@ -1,4 +1,4 @@
-python /efs-storage/fairseq/Kg2tex/model_utils/augment_mbart_model.py \
+python /efs-storage/fairseq/Kg2text/model_utils/augment_mbart_model.py \
    /efs-storage/fairseq/Kg2text/model/mbart50.ft.nn \
    --tgt-dict "/efs-storage/fairseq/Kg2text/data-bin/dict.mbart50_wtags.txt" \
    --save-to "/efs-storage/fairseq/Kg2text/model/mbart50.ft.nn/model_wtags" \
