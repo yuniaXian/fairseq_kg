c7fe0e8c7ee2a04e482bfebe7e9d0a8e5b6cd773
Ubuntu <ubuntu@ip-172-31-83-232.ec2.internal>
2021-06-13 23:10:25 +0000

    6

--- Kg2text/code/kg_denoising_pretrain_aws.sh
+++ Kg2text/code/kg_denoising_pretrain_aws.sh
@@ -11,7 +11,7 @@ TOKENIZER=${EFS}/tokenizer
 # NAME=webnlg/data_mbart50_wtags
 DATADIR=${BASE}/dataset_denoising/kgtext_wikidata
 PRETRAIN=${EFS}/models/mbart50.ft.nn/model_wtags0/model.pt
-tensorboard_dir=${EFS}/logs/tensorboard/denoising_kgtext_wikidata
+tensorboard_dir=$BASE/logs/tensorboard/denoising_kgtext_wikidata
 
 #python ${FAIRSEQ}/train.py ${DATADIR} \
 CUDA_VISIBLE_DEVICES=${CUDA} python ${FAIRSEQ}/train.py ${DATADIR} \
@@ -22,7 +22,7 @@ CUDA_VISIBLE_DEVICES=${CUDA} python ${FAIRSEQ}/train.py ${DATADIR} \
     --lr-scheduler polynomial_decay --lr "3e-05" --stop-min-lr "-1"  \
     --warmup-updates 2500 --max-update 40000 --total-num-update 40000  \
     --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0  \
-    --max-tokens 1024 --update-freq 1 --save-interval 1  \
+    --max-tokens 2048 --update-freq 1 --save-interval 1  \
     --save-interval-updates 8000 --keep-interval-updates 10 --no-epoch-checkpoints --seed 222  \
     --log-format simple --log-interval 2 --save-dir checkpoint/denoising_kgtext_wikidata  \
     --layernorm-embedding --ddp-backend no_c10d --langs en_XX --no-whole-word-mask-langs False  \
