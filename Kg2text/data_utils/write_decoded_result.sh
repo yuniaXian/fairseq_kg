e32f223fabf31451096a837f7977d2db547f1ce2
Ubuntu <ubuntu@ip-172-31-83-232.ec2.internal>
2021-06-16 22:51:06 +0000

    21

--- Kg2text/data_utils/write_decoded_result.sh
+++ Kg2text/data_utils/write_decoded_result.sh
@@ -1,17 +1,22 @@
-log_base=/home/ubuntu/logs
+log_base=/home/ubuntu/efs-storage/decoded_results
+pretrain_setting=ft_mbart50/mask_15_mixed_span_35
+dataset=webnlg
+file_name=chp_best.log
+
+load_file_dir=$log_base/$pretrain_setting/ft_$dataset/
 nohup_output=$1
 decoded_output=$2
-tags=[en_XX]
+tags=[en_XX][TEXT]
 
 
-cat $log_base/$nohup_output | grep -P "^D" |sort -V |cut -f 3- | cat > $log_base/$decoded_output.hyp
-cat $log_base/$nohup_output | grep -P "^T" |sort -V |cut -f 2- | cat > $log_base/$decoded_output.ref
+cat $load_file_dir/$nohup_output | grep -P "^D" |sort -V |cut -f 3- | cat > $load_file_dir/$decoded_output.hyp
+#cat $load_file_dir/$nohup_output | grep -P "^T" |sort -V |cut -f 2- | cat > $load_file_dir/$decoded_output.ref
 python "/home/ubuntu/fairseq/Kg2text/data_utils/del_tags.py" \
-    --load_file $log_base/$decoded_output.hyp --save_file  $log_base/$decoded_output.hyp \
+    --load_file $load_file_dir/$decoded_output.hyp --save_file  $load_file_dir/$decoded_output.hyp \
     --tags_to_del $tags
 
-python "/home/ubuntu/fairseq/Kg2text/data_utils/del_tags.py" \
-    --load_file $log_base/$decoded_output.ref --save_file  $log_base/$decoded_output.ref \
-    --tags_to_del $tags
+#python "/home/ubuntu/fairseq/Kg2text/data_utils/del_tags.py" \
+#    --load_file $log_base/$decoded_output.ref --save_file  $log_base/$decoded_output.ref \
+#    --tags_to_del $tags
 
 #./measure_scores.py example-inputs/devel-conc.txt example-inputs/baseline-output.txt
