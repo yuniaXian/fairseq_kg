0417ef66964db8a14ef0eef6b99e9b9ee0d52450
Ubuntu <ubuntu@ip-172-31-91-234.ec2.internal>
2021-06-17 09:20:37 +0000

    fs_m

--- Kg2text/experiment/script_template/kg_denoising_pretrain_aws.sh
+++ Kg2text/experiment/script_template/kg_denoising_pretrain_aws.sh
@@ -10,11 +10,16 @@ TOKENIZER=${EFS}/tokenizer
 #langs=af_ZA,ar_AR,az_AZ,bn_IN,cs_CZ,de_DE,en_XX,es_XX,et_EE,fa_IR,fi_FI,fr_XX,gl_ES,gu_IN,he_IL,hi_IN,hr_HR,id_ID,it_IT,iu_CA,ja_JP,ja_XX,ka_GE,kk_KZ,km_KH,ko_KR,lt_LT,lv_LV,mk_MK,ml_IN,mn_MN,mr_IN,my_MM,ne_NP,nl_XX,pl_PL,ps_AF,pt_XX,ro_RO,ru_RU,si_LK,sl_SI,sv_SE,ta_IN,te_IN,th_TH,tr_TR,uk_UA,ur_PK,vi_VN,xh_ZA,zh_CN
 #langs_25=ar_AR,cs_CZ,de_DE,en_XX,es_XX,et_EE,fi_FI,fr_XX,gu_IN,hi_IN,it_IT,ja_XX,kk_KZ,ko_KR,lt_LT,lv_LV,my_MM,ne_NP,nl_XX,ro_RO,ru_RU,si_LK,tr_TR,vi_VN,zh_CN
 # NAME=webnlg/data_mbart50_wtags
+
+
+#pretrain_setting=ft_mbart50/mask_15_mixed_span_35
+pretrain_setting=fs_mbart50/mask_15_mixed_span_35
+#pretrain_setting=fs_mbart50/mask_15_word_word
 DATADIR=${BASE}/dataset_denoising/kgtext_wikidata
 PRETRAIN=${EFS}/models/mbart50.ft.nn/model_wtags0/model.pt
-tensorboard_dir=$BASE/logs/tensorboard/denoising_kgtext_wikidata/fs_mbart50_mask_15_word_word
-checkpoint_dir=$BASE/checkpoints/denoising_kgtext_wikidata
-restore_file=$checkpoint_dir/fs_mbart50/mask_15_word_word/checkpoint2.pt
+tensorboard_dir=$BASE/logs/tensorboard/denoising_kgtext_wikidata/$pretrain_setting
+checkpoint_dir=$BASE/checkpoints/denoising_kgtext_wikidata/$pretrain_setting
+restore_file=$checkpoint_dir/checkpoint_1_65000.pt
 
 source /home/ubuntu/anaconda3/bin/activate pytorch_latest_p37
 #python ${FAIRSEQ}/train.py ${DATADIR} \
