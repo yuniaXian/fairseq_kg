1607a2f9a0a3e20daa8d63bcfa46e4d0ec995bd1
Ubuntu <ubuntu@ip-172-31-80-200.ec2.internal>
2021-06-17 08:21:04 +0000

    experiment fs_mask_w_w

--- Kg2text/experiment/script_template/generate_.sh
+++ Kg2text/experiment/script_template/generate_.sh
@@ -6,17 +6,18 @@ FAIRSEQ=${WORKSPACE}/fairseq/fairseq_cli
 KG2TEXT=${WORKSPACE}/fairseq/Kg2text
 TOKENIZER=${EFS}/tokenizer
 
-pretrain_setting=ft_mbart50/mask_15_mixed_span_35
+#pretrain_setting=ft_mbart50/mask_15_mixed_span_35
 #pretrain_setting=fs_mbart50/mask_15_mixed_span_35
-#pretrain_setting=fs_mbart50/mask_15_word_word
+pretrain_setting=fs_mbart50/mask_15_word_word
 checkpoint_file=checkpoint_best.pt
 dataset=webnlg
 
-PRETRAIN=${EFS}/checkpoints/denoising_kgtext_wikidata/$pretrain_setting/ft_$dataset/$checkpoint_file
+PRETRAIN=${BASE}/checkpoints/denoising_kgtext_wikidata/$pretrain_setting/ft_$dataset/$checkpoint_file
 DATADIR=${EFS}/data-bin/dataset_kg2text/$dataset/en_XX
 sentencepiece_model=${BASE}/efs-storage/tokenizer/mbart50/bpe/sentence.bpe.model
 
 fairseq-generate $DATADIR \
   --path $PRETRAIN \
   --bpe 'sentencepiece' --sentencepiece-model ${sentencepiece_model} \
-  --beam 5
+  --beam 5 \
+  --num-workers 64
